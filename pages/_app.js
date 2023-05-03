6e039271e28fd087e167fa7edb467798cd660560
Vinit Patil <patilvinit83@gmail.com>
2023-05-03 18:49:19 +0530

    front-end 1.0

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import { Navbar, Footer } from "../components";
+import { StateContext } from "@/context/StateContext";
+import { Toaster } from "react-hot-toast";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Toaster />
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </StateContext>
+  );
 }
